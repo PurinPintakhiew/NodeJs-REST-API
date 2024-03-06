aa586e0404b406015fbadf44d3653ac88e6d1465
PurinCompattana <dev.purin@gmail.com>
2024-03-07 00:13:01 +0700

    chore: add update validate

--- validations/validates/UserValidate.js
+++ validations/validates/UserValidate.js
@@ -4,6 +4,11 @@ exports.show = [
     param('id', 'This user id is not null').notEmpty(),
 ];
 
+exports.update = [
+    body('name', 'This name is not null').notEmpty(),
+];
+
+
 exports.destroy = [
     param('id', 'This user id is not null').notEmpty(),
 ];
