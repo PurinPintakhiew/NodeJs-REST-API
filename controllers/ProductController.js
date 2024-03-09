b73d53fa4df702df7acb1b559d213bd8feac9c47
PurinCompattana <dev.purin@gmail.com>
2024-03-09 22:55:18 +0700

    chore: change function to class

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,29 +1,33 @@
 const Product = require('../models/Product');
 
-exports.All = async (req, res, next) => {
-    try {
-        const products = await Product.findAll();
-        if (products) {
-            return res.status(200).json({
-                status: true,
-                data: products
-            });
+class ProductController {
+    async getAll(req, res, next) {
+        try {
+            const products = await Product.findAll();
+            if (products) {
+                return res.status(200).json({
+                    status: true,
+                    data: products
+                });
+            }
+        } catch (err) {
+            console.error(err)
+            return res.status(500).json({ error: 'Internal Server Error' });
         }
-    } catch (err) {
-        console.error(err)
-        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+    async store(req, req, next) {
+        try {
+            const name = req.body.name;
+            const price = req.body.price;
+            const qty = req.body.qty;
+
+
+        } catch (err) {
+            console.error(err)
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
     }
 }
 
-exports.Store = async (req, req, next) => {
-    try {
-        const name = req.body.name;
-        const price = req.body.price;
-        const qty = req.body.qty;
-        
-        
-    } catch (err) {
-        console.error(err)
-        return res.status(500).json({ error: 'Internal Server Error' });
-    }
-}
+module.exports = new ProductController();
