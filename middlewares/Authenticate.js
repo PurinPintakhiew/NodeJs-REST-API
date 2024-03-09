ed955c2a21b42073539f706ddc6d4094e7bc4df5
PurinCompattana <dev.purin@gmail.com>
2024-03-09 23:24:44 +0700

    chore: change function to class

--- middlewares/Authenticate.js
+++ middlewares/Authenticate.js
@@ -1,17 +1,32 @@
 const jwt = require('jsonwebtoken');
 
-const Authenticate = (req, res, next) => {
-    const authorization = req.headers['authorization'];
-    if (authorization) {
-        const token = authorization.split(' ')[1];
-        if (token === null) return res.status(404).json({ status: false, error: 'Token is null' });
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, result) => {
-            if (err) return res.status(403).json({ status: false, error: err });
-            next();
-        });
-    } else {
-        return res.status(404).json({ status: false, error: 'authorization is null!' });
+class AuthenticationMiddleware {
+    constructor(options) {
+        this.options = options || {};
+    }
+
+    authenticate(req, res, next) {
+        const authorization = req.headers['authorization'];
+
+        if (authorization) {
+            const token = authorization.split(' ')[1];
+
+            if (token === null) {
+                return res.status(404).json({ status: false, error: 'Token is null' });
+            }
+
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, result) => {
+                if (err) {
+                    return res.status(403).json({ status: false, error: err });
+                }
+                next();
+            });
+        } else {
+            return res.status(404).json({ status: false, error: 'Authorization header is null!' });
+        }
     }
 }
 
-module.exports = Authenticate;
+const authenticationMiddleware = new AuthenticationMiddleware();
+
+module.exports = authenticationMiddleware.authenticate.bind(authenticationMiddleware);
