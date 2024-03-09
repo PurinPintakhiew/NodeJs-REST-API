a7ce55945d28d21df9854934ec835227440fe96c
PurinCompattana <dev.purin@gmail.com>
2024-03-09 22:41:35 +0700

    chore: add PORT

--- app.js
+++ app.js
@@ -30,4 +30,7 @@ app.use('/api', Middlewares, UserRoute);
 app.use(ErrorController.get404)
 
 // port
-app.listen(8080)
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
