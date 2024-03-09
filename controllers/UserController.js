b94cd9d3364288f9f1fcb8e5effb8c7561dabd00
PurinCompattana <dev.purin@gmail.com>
2024-03-09 22:42:12 +0700

    chore: change function to class

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,69 +1,64 @@
-const { where } = require("sequelize");
-const User = require("../models/User");
+const User = require('../models/User');
 
-exports.All = async (req, res, next) => {
-    try {
-        const users = await User.findAll();
-        if (users) {
+class UserController {
+    async getAll(req, res, next) {
+        try {
+            const users = await User.findAll();
             return res.status(200).json({
                 status: true,
-                data: users
+                data: users,
             });
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Internal Server Error' });
         }
-    } catch (err) {
-        console.error(err)
-        return res.status(500).json({ error: 'Internal Server Error' });
     }
-}
 
-exports.Update = async (req, res, next) => {
-    try {
-        const id = req.params.id;
-        const name = req.body.name;
+    async update(req, res, next) {
+        try {
+            const id = req.params.id;
+            const name = req.body.name;
 
-        const update = await User.update({ name: name }, { where: { id: id } });
+            const [updatedRows] = await User.update({ name }, { where: { id } });
 
-        if (update) {
-            return res.status(200).json({
-                status: true,
-                message: 'Update successful'
-            });
-        } else {
-            return res.status(200).json({
-                status: false,
-                message: 'Update fail'
-            });
-        }
-
-    } catch (err) {
-        console.error(err)
-        return res.status(500).json({ error: 'Internal Server Error' });
-    }
-}
-
-exports.destroy = async (req, res, next) => {
-    try {
-        const id = req.params.id;
-        const destroy = await User.destroy({
-            where: {
-                id: id
+            if (updatedRows > 0) {
+                return res.status(200).json({
+                    status: true,
+                    message: 'Update successful',
+                });
+            } else {
+                return res.status(200).json({
+                    status: false,
+                    message: 'Update fail',
+                });
             }
-        })
-
-        if (destroy) {
-            return res.status(200).json({
-                status: true,
-                message: 'Destroy successful'
-            });
-        } else {
-            return res.status(200).json({
-                status: false,
-                message: 'Destroy fail'
-            });
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Internal Server Error' });
         }
+    }
 
-    } catch (err) {
-        console.error(err)
-        return res.status(500).json({ error: 'Internal Server Error' });
+    async destroy(req, res, next) {
+        try {
+            const id = req.params.id;
+            const destroyedRows = await User.destroy({ where: { id } });
+
+            if (destroyedRows > 0) {
+                return res.status(200).json({
+                    status: true,
+                    message: 'Destroy successful',
+                });
+            } else {
+                return res.status(200).json({
+                    status: false,
+                    message: 'Destroy fail',
+                });
+            }
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
     }
 }
+
+module.exports = new UserController();
