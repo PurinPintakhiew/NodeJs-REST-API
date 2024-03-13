300e88fa7c899b193a5ed0b616115d3b48eccd80
PurinPintakhiew <dev.purin@gmail.com>
2024-03-13 23:59:40 +0700

    feat: create cart model

---
+++ models/Cart.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize');
+const db = require('../configs/database');
+
+const Cart = db.define('cart', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'users',
+            key: 'id'
+        }
+    },
+    createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+    }
+});
+
+module.exports = Cart;
