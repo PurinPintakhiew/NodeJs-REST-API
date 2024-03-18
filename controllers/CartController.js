c5f40f49a691178fdb4917a884c5199be324a73b
PurinPintakhiew <dev.purin@gmail.com>
2024-03-18 23:51:41 +0700

    chore: add function in CartController

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -1,5 +1,44 @@
+const Cart = require('../models/Cart')
+
 class CartController {
-    
+    async getAll(req, res, next) {
+        try {
+            const carts = await Cart.findAll();
+            if (carts) {
+                return res.status(200).json({
+                    status: true,
+                    data: carts
+                });
+            }
+        } catch (err) {
+            console.error(err)
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
+
+    async show(req, res, next) {
+        try {
+            const id = req.params.id;
+
+            const cart = await Cart.findOne({ where: { id: id } });
+            if (cart) {
+                return res.status(200).json({
+                    status: true,
+                    data: cart
+                });
+            } else {
+                return res.status(404).json({
+                    status: true,
+                    data: cart,
+                    message: 'data is not found'
+                });
+            }
+        } catch (err) {
+            console.error(err)
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
+
 }
 
 module.exports = new CartController;
